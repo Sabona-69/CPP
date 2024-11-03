ffefd1da07665cac6cd5d0fc2cf06942c41bda0a
Hamza El Omrani <hel-omra@e3r1p17.1337.ma>
2024-11-03 06:37:07 +0100

    /latest update/

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,29 @@
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
+#include <iostream>
+
+int	main()
+{
+	std::string	input;
+	PhoneBook	Book;
+	while (1)
+	{
+		std::cout << "Insert command >: ";
+		if (!std::getline(std::cin, input))// || std::cin.eof())
+			break;
+		if (input == "ADD")
+			Book.set_contact();
+		else if (input == "SEARCH")
+		{
+			Book.get_table();
+			for (int i = 0; i < Book.get_count() - 1; i++)
+				Book.get_contact();
+			Book.get_contact_info();
+		}
+		else if (input == "EXIT")
+			return 0;
+		else {
+			std::cout << "Please Insert right command (ADD, SEARCH or EXIT)" << std::endl;
+		}
+	}	
+}
