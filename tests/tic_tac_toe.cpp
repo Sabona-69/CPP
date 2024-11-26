b9517474ae3e9e07f637fcf24a9d24dea31cdaff
Hamza El Omrani <hel-omra@e3r1p1.1337.ma>
2024-11-26 06:50:31 +0100

    /latest update/

--- tests/tic_tac_toe.cpp
+++ tests/tic_tac_toe.cpp
@@ -5,46 +5,53 @@ class Tic
 {
 	public:
 		char	player;
-		char	*table[4] = {
-			"   ",
-			"   ", 
-			"   ",
-			NULL
-		};
+		char	**table;
 		std::string level;
-		// void create_table(){
-		// 	for (int i = 0; i < 9; i++)
-		// 	{
-		// 		// table[i] = ;
-		// 		for (int j = 0; j < 10; j++)
-		// 			table[i][j] = ;
-		// 	}
-		// }
+		void create_table(){
+			table = (char **)malloc(sizeof(char *) * 4);
+			int k = 1;
+			for (int i = 0; i < 3; i++)
+			{
+				table[i] = (char *)malloc(4);
+				for (int j = 0; j < 3; j++)
+					table[i][j] = k++ + 48;
+			}
+		}
 		void print_table()
 		{
-			// create_tabke();
-			std::cout << "-------------\n";
+			create_table();
+			std::cout << "----------\n";
 			for (int i = 0; i < 3; i++)
 			{
 				for (int j = 0; j < 3; j++)
 				{
-					std::cout << "|" << std::setw(2) << std::left;
-					// if (table[i][j])
-						std::cout << table[i][j];
-					// else 
-						std::cout << " ";
+					std::cout << "|" << std::setw(2) << std::left << table[i][j];
 				}
-					// std::cout << std::endl;
-			std::cout << "|\n-------------\n";
+			std::cout << "|\n----------\n";
 			}
 		}
+		bool	check_win(char c, int i, int j)
+		{
+			
+		}
+		int		you_won()
+		{
+			for (int i = 0; table[i]; i++){
 
+				for (int j = 0; table[i][j]; j++){
+					if (table[i][j] == 'X' || table[i][j] == 'O')
+					{
+						if (check_win(table[i][j], i, j));
+					}
+				}
+			}
+
+		}
 };
 
 int main()
 {
 	Tic game;
-	game.print_table();
 	std::string input;
 	while (true)
 	{
@@ -52,13 +59,19 @@ int main()
 		std::cout << "1 : for X" << std::endl;
 		std::cout << "2 : for O" << std::endl;
 		getline(std::cin, input);
-		{
 		while (input != "1" && input != "2")
+		{
 			std::cout << "Wrong Choice :" << std::endl;
 			getline(std::cin, input);
 		}
-		game.player = input[0];
-
+		if (input == "1")
+			game.player = 'X';
+		else 
+			game.player = 'O';
+		std::cout << "You Choosed : " << game.player << std::endl;
+		while (!game.you_won())
+			game.print_table();
+		
 		std::cout << input << std::endl;
 	}
 }
