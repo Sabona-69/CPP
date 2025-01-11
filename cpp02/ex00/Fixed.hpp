a6d4460354cc04d40924ad3892cf54fc01da2d58
Hamza El Omrani <hel-omra@e2r2p9.1337.ma>
2025-01-11 15:05:55 +0100

    latest

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,9 +6,15 @@
 
 class Fixed{
     private:
-
+        int                fixedNumber;
+        static const int    frac;
     public:
-        
+        Fixed();
+        Fixed(const Fixed& inst);
+        ~Fixed();
+        Fixed& operator=(const Fixed& inst);
+        int getRawBite( void ) const;
+        void setRawBite( int const raw ) ;
 };
 
 #endif // !FIXED_HPP
