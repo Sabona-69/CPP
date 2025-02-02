6fa348eff08000bf1eddecc2b9f6caec514ac6fa
Sabona-69 <hamza.elomrani111@gmail.com>
2025-02-02 19:55:26 +0100

    latest

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -1,46 +1,38 @@
 #include "Harl.hpp"
 
-void Harl::debug() { std::cout << "Debugging !" << std::endl; }
-
-void Harl::info() { std::cout << "Some info here !" << std::endl; }
-
-void Harl::warning() {
-  std::cout << "Carefull this is a warning !" << std::endl;
+void Harl::debug() { 
+	std::cout << "{ DEBUG }\nDebugging !\n" << std::endl;
 }
 
-void Harl::error() { std::cout << "ERROR !" << std::endl; }
+void Harl::info() {
+	 std::cout << "{ INFO }\nSome info here !\n" << std::endl;
+}
+
+void Harl::warning() {
+	std::cout << "{ WARNING }\nCarefull this is a warning !\n" << std::endl;
+}
+
+void Harl::error() {
+	std::cout << "{ ERROR }\n This is unacceptable! I want to speak to the manager now !\n" << std::endl;
+}
 
 void Harl::complain(std::string level) {
-  std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-  int index = -1;
-  void (Harl::*func[])() = {&Harl::debug, &Harl::info, &Harl::warning,
-                            &Harl::error};
-  for (int i = 0; i < 4; i++) {
-    if (levels[i] == level)
-      index = i;
-  }
- for (int i = index; i < 4; i++) {
-    switch (index) {
-    case 0: {
-      (this->*func[i])();
-      break;
-    }
-    case 1: {
-      (this->*func[i])();
-      break;
-    }
-    case 2: {
-      (this->*func[i])();
-      break;
-    }
-    case 3: {
-      (this->*func[i])();
-      break;
-    }
-    default: {
-      std::cout << "[ Probably complaining about insignificant problems ]"
-                << std::endl;
-    }
-   }
+	int count = (level == "DEBUG") + (level == "INFO") * 2 + (level == "WARNING") * 3 + (level == "ERROR") * 4;
+	switch (count) {
+    case 1:
+      debug();
+      // fallthrough
+    case 2:
+      info();
+      // fallthrough
+    case 3:
+      warning();
+      // fallthrough
+    case 4:
+      error();
+      // fallthrough
+	  break;
+    default:
+      std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
   }
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -1,11 +1,12 @@
 #include "Harl.hpp"
 
-int main () {
+int main (int ac, char **av) {
     Harl test;
 
-    test.complain("DEBUG");
-    // test.complain("ERROR");
-    // test.complain("DEBUG");
-    // test.complain("DEBUG");
+	if (ac != 2){
+		std::cerr << "Invalid Arguments !" << std::endl;
+		return 1;
+	}
+    test.complain(av[1]);
     return 0;
 }
