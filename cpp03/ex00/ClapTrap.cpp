b7a1818b568e9775c457e530f6b3d8211ba16a74
Sabona-69 <hamza.elomrani111@gmail.com>
2025-02-25 16:25:41 +0000

    latest

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,36 @@
+#include "ClapTrap.hpp"
+
+ClapTrap:: ClapTrap(){
+    // this->name = "";
+}
+
+ClapTrap:: ClapTrap(const ClapTrap &assign){
+    
+}
+
+ClapTrap:: ClapTrap(const std::string name){
+    this->name = name;
+    this->health = 10;
+    this->energy = 10;
+    this->damage = 0;
+}
+
+ClapTrap:: ~ClapTrap(){
+
+}
+
+ClapTrap   ClapTrap::operator=(const ClapTrap &assign){
+
+}
+
+void ClapTrap::attack(const std::string& target){
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount){
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount){
+
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include <iostream>
+
+class ClapTrap{
+    private:
+        std::string name;
+        int         energy;
+        int         health;
+        int         damage;
+    public:
+    ClapTrap();
+    ClapTrap(const ClapTrap &assign);
+    ClapTrap(const std::string name);
+    ~ClapTrap();
+    ClapTrap    operator=(const ClapTrap &assign);
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+};
