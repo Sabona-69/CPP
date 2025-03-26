7d3568fda1d2ef93fd5a44f22a801aa7f05846e7
Sabona-69 <hamza.elomrani111@gmail.com>
2025-03-26 08:50:57 +0000

    latest

--- cpp04/ex03/Character.hpp
+++ cpp04/ex03/Character.hpp
@@ -5,9 +5,13 @@
 #include "ICharacter.hpp"
 
 class Character : public ICharacter {
+    private:
+        std::string name;
+        AMateria* inventory[4];
     public:
         Character();
         Character(const Character &assign);
+        Character(const std::string &assign);
         Character&   operator=(const Character &assign);
         ~Character();
 };
--- cpp04/ex03/ICharacter.cpp
+++ cpp04/ex03/ICharacter.cpp
@@ -1,7 +1,6 @@
 #include "ICharacter.hpp"
 
 ICharacter:: ICharacter(){
-
 }
 
 ICharacter:: ICharacter(const ICharacter &assign){
