7bb49d314105f2ec974679e2b4cda5554d59a5f8
Sabona-69 <hamza.elomrani111@gmail.com>
2025-04-14 21:46:53 +0100

    latest

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -2,18 +2,27 @@
 #define ARRAY_HPP
 
 #include <iostream>
+#include <exception>
 
-template<typename T , typename N>
+
+template<typename T>
 
 class Array {
     private:
-        T arr[N];
+        T   *data;
+        unsigned int  d_size;
     public:
+        Array(unsigned int n);
         Array();
         ~Array();
         Array(const Array &assign);
-        Array& operator=(const Array &assign);
-        Array(N n);
+        T& operator=(const Array &assign);
+        T& operator[](unsigned int n);
+        class ThrowInvalidIndex : public std::exception {
+            const char *what() const throw();
+        };
+        unsigned int size();
 };
 
 #endif
+    
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include "Array.hpp"
 
-int main(int argc, char const *argv[])
-{
+
+int main() {
+    // Create array of 5 integers
+    Array<int> int_arr(5);
     
+    // Initialize values
+    for(size_t i = 0; i < int_arr.size(); ++i) {
+        int_arr[i] = i * 10;
+    }
+
+    // Test copy constructor
+    Array<int> int_arr2 = int_arr;
+    int_arr2[0] = 100;
+
+    // Test assignment operator
+    Array<int> int_arr3(3);
+    int_arr3 = int_arr;
+
+    // Output results
+    std::cout << "Original[0]: " << int_arr[0]   // 0
+              << " Copy[0]: " << int_arr2[0]     // 100
+              << " Assign[0]: " << int_arr3[0];  // 0
+
     return 0;
 }
