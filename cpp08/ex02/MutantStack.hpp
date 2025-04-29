90c2ad50a2ee179bfbb328b5273cf5d728d85830
Sabona-69 <hamza.elomrani111@gmail.com>
2025-04-29 18:35:23 +0100

    latest

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,25 +2,46 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
 
 
 template <typename T>
-class MutantStack {
-    private: 
-        T arr;
+class MutantStack : public std::stack<T> {
+    // private: 
+    //     std::stack<T> arr; 
     public:
+        typedef typename std::stack<T>::container_type::iterator iterator;
         MutantStack();
         ~MutantStack();
         MutantStack(const MutantStack &assign);
         MutantStack& operator=(const MutantStack &assign);
-        void push(const T& add);
-        class iterator
-        {
-            private:
-            public:
-                T* begin();
-                T* end();
-        };
-};
+        iterator begin();
+        iterator  end();
+        // void 
+        
+    };
+template <typename T>
+std::stack<T>::MutantStack::MutantStack(){
+
+}
+    
+MutantStack::~MutantStack(){
+
+}
+MutantStack::MutantStack(const MutantStack &assign){
+
+}
+MutantStack::MutantStack& operator=(const MutantStack &assign){
+
+}
+template <typename T>
+iterator MutantStack::begin(){
+
+}
+iterator MutantStack::end(){
+
+}
 
 #endif
