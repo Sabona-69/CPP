5ba5889bdddf5d24dec06089df519473028ba4ef
sabona-69 <hamza.elomrani111@gmail.com>
2025-09-22 07:39:45 +0100

    fixing span

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,13 +6,13 @@
 
 
 template<typename T>
-typename T::iterator    easyfind(T& t, int i)
+typename T::iterator	easyfind(T& t, int i)
 {
-    typename T::iterator it = std::find(t.begin(), t.end(), i);
-    if (it == t.end()) {
-        throw std::runtime_error("Value not founded!");
-    }
-    return it;
+	typename T::iterator it = std::find(t.begin(), t.end(), i);
+	if (it == t.end()) {
+		throw std::runtime_error("Value not founded!");
+	}
+	return it;
 }
 
 #endif
