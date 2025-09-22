614979afcfc7a59c79670df2bbdc932e9b03ec98
sabona-69 <hamza.elomrani111@gmail.com>
2025-09-22 17:59:04 +0100

    latest

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -13,12 +13,14 @@ int main()
 		a.push_back(2);
 		a.push_back(3);
 		a.push_back(4);
-		int	 find = 4;		
+		int	 find = 4;
+
 		easyfind(a, find);
 		std::cout <<  find << " founded in a container!\n";
 		// b.push_back(4);
 		easyfind(b, find);
 		std::cout <<  find << " founded in b container!\n";
+
 	}
 	catch(const std::exception& e)
 	{
